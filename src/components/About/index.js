e763ba2c8ac637f39a0b8dec01203bcddf3593c5
sachindra-r <rathnayake.sachindra@gmail.com>
2022-11-02 22:24:38 +1100

    Minor Change to data

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -41,18 +41,13 @@ const About = () => {
             />
           </h1>
           <p>
-            I'm a very ambitious front-end developer looking for a role in an
+            I'm a very ambitious fullstack developer looking for a role in an
             established IT company with the opportunity to work with the latest
             technologies on challenging and diverse projects.
           </p>
           <p align="LEFT">
             I'm quiet confident, naturally curious, and perpetually working on
-            improving my chops one design problem at a time.
-          </p>
-          <p>
-            If I need to define myself in one sentence that would be a family
-            person, father of a beautiful daughter, a sports fanatic,
-            photography enthusiast, and tech-obsessed!!!
+            improving my skills and at simultaneously doing some amazing work as well.
           </p>
         </div>
 
